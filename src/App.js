33780bcb1de74989ae39c2553edefe38aee5f2f7
Angelo Pavilando <pavilandoangelo@gmail.com>
2021-01-04 16:21:39 +0800

    ✨ Todo List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container h-screen mx-auto px-4 bg-yellow-200">
+      <div className="flex flex-row h-full w-full justify-center items-center">
+        <div className="flex flex-col h-2/3 w-1/3 bg-gray-200 text-center content-center p-10 rounded-lg">
+          <div className="flex flex-row h-12 space-x-2">
+            <input className="flex-grow h-full" />
+            <span className="flex-shrink h-full">Button</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
