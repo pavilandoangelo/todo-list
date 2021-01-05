237d1e746dd3eb4a6fd4d55ab168e4b09c8490b0
Angelo Pavilando <pavilandoangelo@gmail.com>
2021-01-05 16:49:09 +0800

    ✅ Update UI

--- src/App.js
+++ src/App.js
@@ -3,17 +3,104 @@ function App() {
     <div className="container h-screen mx-auto px-4 bg-yellow-200 font-sans subpixel-antialiased">
       <div className="flex flex-row h-full w-full justify-center items-center">
         <div className="flex flex-col h-2/3 w-1/3 bg-gray-200 text-center content-center p-10 rounded-lg space-y-5">
-          <div className="flex flex-row h-12 w-full space-x-4">
+          <div className="flex flex-row h-12 w-full space-x-2">
             <input className="flex-grow h-full w-11/12 focus:outline-none focus:ring focus:ring-blue-200 text-xl px-2" />
-            <button className="flex-initial h-full bg-green-500 rounded-full focus:outline-none focus:ring focus:ring-blue-200 ">
+            <button className="flex-initial h-full p-2 bg-green-500 focus:outline-none focus:ring focus:ring-blue-200 ">
               <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
               </svg>
             </button>
           </div>
-          <div className="h-3/4 w-full grid grid-cols-1">
-            <div>1</div>
-            <div>2</div>
+          <div className="w-full flex flex-col justify-start space-y-2 overflow-y-scroll">
+            <div className="w-full flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div><div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+            <div className="w-full h-10 flex flex-row">
+              <div className="flex-grow h-full w-11/12 bg-white text-center p-2 text-lg font-normal tracking-wide whitespace-normal break-all">
+                <span className="h-auto">Clean Room</span>
+              </div>
+              <button className="flex-initial h-full bg-red-500 p-2 focus:outline-none focus:ring focus:ring-blue-200 ">
+                <svg className="h-full fill text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </div>
